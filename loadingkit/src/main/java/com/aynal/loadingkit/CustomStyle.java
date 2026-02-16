9006ca7faff024b02f2767f1e85514f7864f48f3
MD Aynal Haque <145339888+aynalhaque6@users.noreply.github.com>
2026-02-16 10:13:51 +0600

    Delete loadingkit directory

--- loadingkit/src/main/java/com/aynal/loadingkit/CustomStyle.java
+++
@@ -1,9 +0,0 @@
-package com.aynal.loadingkit;
-
-/** Built-in custom loader styles (no external libraries). */
-public enum CustomStyle {
-    RING_PULSE,
-    BARS_WAVE,
-    DOTS_ORBIT,
-    ARC_SPINNER
-}
--- loadingkit/src/main/java/com/aynal/loadingkit/ui/LoadingOverlayView.java
+++
@@ -1,4 +0,0 @@
-package com.aynal.loadingkit.ui;
-
-public class LoadingOverlayView {
-}
